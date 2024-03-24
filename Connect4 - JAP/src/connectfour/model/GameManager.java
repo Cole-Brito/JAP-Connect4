388f40c0a167b5f5ecdf9c14720890fc667974b0
Paul Squires <squi0031@algonquinlive.com>
2024-03-24 15:16:29 -0400

    Added some JavaDoc comments to GameManager

--- Connect4 - JAP/src/connectfour/model/GameManager.java
+++ Connect4 - JAP/src/connectfour/model/GameManager.java
@@ -6,7 +6,13 @@ import java.util.Timer;
 
 public class GameManager {
 	
+	/** The singleton instance of GameManager */
 	private static GameManager _instance;
+	/**
+	 * Gets the singleton instance of GameManager.
+	 * Constructs a new GameManager only the first time it is called.
+	 * @return The instance of GameManager
+	 */
 	public static GameManager getInstance() {
 		if (_instance == null) {
 			_instance = new GameManager();
@@ -24,8 +30,14 @@ public class GameManager {
 	private Player player2;
 	public Player activePlayer;
 	
+	/** PropertyChangeSupport for notifying PropertyChangeListeners of updated model */
 	private final PropertyChangeSupport propertyChangedSupport;
 	
+	/**
+	 * Private constructor for GameManager singleton.
+	 * Sets initial states.
+	 * Depends on PlayerManager being initialized.
+	 */
 	private GameManager() {
 		var players = PlayerManager.getInstance().getPlayers();
 		assert(players.size() >= 2);
@@ -36,6 +48,14 @@ public class GameManager {
 		propertyChangedSupport = new PropertyChangeSupport(this);
 	}
 	
+	/**
+	 * Attempts to place a tile in the given column.
+	 * @param row Is ignored, as tiles will attempt to place in the first open row
+	 * @param column The column to place the tile in
+	 * @param activePlayer Which player is attempting to place a tile. Will only allow placement
+	 * 		if the player is the same as the game's current activePlayer
+	 * @return true if a tile was placed, false if denied for any reason (board full, wrong player, etc.)
+	 */
 	public boolean tryPlaceTile(int row, int column, Player activePlayer) {
 		
 		int currentPlayer = gameState.getPlayerID();
@@ -103,7 +123,7 @@ public class GameManager {
 	 * @return
 	 */
 	public boolean saveGame(String file) {
-		
+		//TODO: implement this method
 		return false;
 	}
 	/**
@@ -112,10 +132,15 @@ public class GameManager {
 	 * @return
 	 */
 	public boolean loadGame(String file) {
-		
+		//TODO: implement this method
 		return false;
 	}
 	
+	/**
+	 * Updates the game state. Notifies PropertyChangeListeners if the new state
+	 * is different than the old state.
+	 * @param state The new game state
+	 */
 	private void updateGameState(GameState state) {
 		var oldState = gameState;
 		if (state != gameState) {
@@ -124,6 +149,10 @@ public class GameManager {
 		}
 	}
 	
+	/**
+	 * Swaps active players between Player1 and Player2.
+	 * Updates GameState.
+	 */
 	private void switchPlayers() {
 		if (activePlayer == player1) {
 			activePlayer = player2;
@@ -154,8 +183,11 @@ public class GameManager {
 	
 	/********** PropertyChanged fields and methods **********/
 	
+	/** Property Name used to notify PropertyChange events when a tile on the game board changes */
 	public static final String GAME_BOARD_TILE_PROPERTY_NAME = "GameBoardTile";
+	/** Property Name used to notify PropertyChange events when the whole game board changes */
 	public static final String GAME_BOARD_FULL_PROPERTY_NAME = "GameBoardFull";
+	/** Property Name used to notify PropertyChange events when the game state changes */
 	public static final String GAME_STATE_PROPERTY_NAME = "GameState";
 	
 	/**
@@ -171,8 +203,8 @@ public class GameManager {
 	
 	/**
 	 * Registers a PropertyChangeListener to responds to changes to GameState.
-	 * Event will be fired when a new message is added.<br>
-	 * Note that the PropertyChangeEvent will use {@link } as old and new values.
+	 * Event will be fired when a Game State changes.<br>
+	 * Note that the PropertyChangeEvent will use {@link GameState} enum as old and new values.
 	 * 
 	 * @param listener The {@link PropertyChangeListener} that responds to GameBoard being changed
 	 */
@@ -180,20 +212,41 @@ public class GameManager {
 		propertyChangedSupport.addPropertyChangeListener(GAME_STATE_PROPERTY_NAME, listener);
 	}
 	
+	/**
+	 * Notify PropertyChangeListeners when a GameBoard tile changes
+	 * @param row The row index of the tile that changed
+	 * @param column The column index of the tile that changed
+	 * @param state The new state of the tile
+	 */
 	public void onGameBoardChanged(int row, int column, int state) {
 		propertyChangedSupport.firePropertyChange(GAME_BOARD_TILE_PROPERTY_NAME, null, 
 			new GameBoardPropertyChangedEvent(row, column, state));
 	}
 	
+	/**
+	 * Notify PropertyChangeListeners when the Game State changes
+	 * @param newState The updated Game State
+	 * @param oldState The previous Game State
+	 */
 	public void onGameStateChanged(GameState newState, GameState oldState) {
 		propertyChangedSupport.firePropertyChange(GAME_STATE_PROPERTY_NAME, oldState, newState);
 	}
 	
+	/**
+	 * Inner class used as oldValue and newValue in PropertyChange events for
+	 * the {@value GameManager#GAME_BOARD_TILE_PROPERTY_NAME} property name.
+	 */
 	public class GameBoardPropertyChangedEvent{
 		public final int row;
 		public final int column;
 		public final int state;
 		
+		/**
+		 * Construct a new GameBoardPropertyChangedEvent
+		 * @param row The row of the tile that updated
+		 * @param column The column of the tile that updated
+		 * @param state The new state of the tile
+		 */
 		public GameBoardPropertyChangedEvent(int row, int column, int state) {
 			this.row = row;
 			this.column = column;
